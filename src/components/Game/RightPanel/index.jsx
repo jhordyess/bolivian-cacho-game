4d3749c5052e8c8695e9fde012a89f635b2788cc
Jhordy Gavinchu <dev@jhordyess.com>
2023-05-17 00:00:01 +0000

    Added roll with frame

--- src/components/Game/RightPanel/index.jsx
+++ src/components/Game/RightPanel/index.jsx
@@ -1,34 +1,46 @@
 import React from "react";
+const classNameOnRolling = "hover: cursor-not-allowed";
 
-const RightPanel = ({ onClose, roll }) => (
-  <div className="border-r border-y flex flex-col justify-between gap-6 py-6 w-40">
-    <div className="flex flex-col gap-4 px-6 w-full">
-      <p className="text-justify">
-        Aute dolore velit magna ipsum duis anim magna consectetur non. Laboris
-      </p>
+const RightPanel = ({ onClose, roll, rolling }) => {
+  return (
+    <div className="border-r border-y flex flex-col justify-between gap-6 py-6 w-40">
+      <div className="flex flex-col gap-4 px-6 w-full">
+        <p className="text-justify">
+          Aute dolore velit magna ipsum duis anim magna consectetur non. Laboris
+        </p>
+      </div>
+      <div className="flex flex-col gap-4 px-6 w-full">
+        <button
+          className={`bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-md ${
+            rolling ? classNameOnRolling : ""
+          }`}
+          title="Roll dices / End turn"
+          disabled={rolling}
+        >
+          START
+        </button>
+        <button
+          className={`bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-md ${
+            rolling ? classNameOnRolling : ""
+          }`}
+          title="Roll dices / End turn"
+          onClick={roll}
+          disabled={rolling}
+        >
+          ROLL
+        </button>
+        <button
+          className={`bg-red-200 hover:bg-red-400 text-white font-bold py-2 px-6 rounded-md ${
+            rolling ? classNameOnRolling : ""
+          }`}
+          onClick={onClose}
+          disabled={rolling}
+        >
+          Exit
+        </button>
+      </div>
     </div>
-    <div className="flex flex-col gap-4 px-6 w-full">
-      <button
-        className="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-md"
-        title="Roll dices / End turn"
-      >
-        START
-      </button>
-      <button
-        className="bg-blue-400 hover:bg-blue-600 text-white font-bold py-2 px-6 rounded-md"
-        title="Roll dices / End turn"
-        onClick={roll}
-      >
-        ROLL
-      </button>
-      <button
-        className="bg-red-200 hover:bg-red-400 text-white font-bold py-2 px-6 rounded-md"
-        onClick={onClose}
-      >
-        Exit
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default RightPanel;
--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -13,21 +13,33 @@ function rollDices() {
 
 export default function Game({ onClose }) {
   const [dices, setDices] = React.useState([0, 0, 0, 0, 0]);
+  const [rolling, setRolling] = React.useState(false);
 
+  let animation;
   const roll = () => {
     setDices(rollDices());
+    animation = window.requestAnimationFrame(roll);
+  };
+
+  const rollingDices = () => {
+    setRolling(true);
+    animation = window.requestAnimationFrame(roll);
+    setTimeout(() => {
+      window.cancelAnimationFrame(animation);
+      setRolling(false);
+    }, 1500);
   };
 
   return (
     <div className="max-h-screen min-w-full overflow-y-auto">
       <div className="fixed inset-0 flex items-center justify-center bg-white gap-4 px-12">
         <div className="flex flex-row">
-          <Board player1="" player2="">
+          <Board player1="" player2="bot">
             <ScoreChart name="Player 2" />
             <Dices dices={dices} />
             <ScoreChart name="Player 1" />
           </Board>
-          <RightPanel onClose={onClose} roll={roll} />
+          <RightPanel onClose={onClose} roll={rollingDices} rolling={rolling} />
         </div>
       </div>
     </div>
