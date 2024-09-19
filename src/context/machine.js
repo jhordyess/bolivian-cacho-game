3ff415085958aa38ba63517cd77e846575dc96a1
jhordyess <dev@jhordyess.com>
2024-09-18 21:16:02 -0400

    chore: Completed machine

--- src/context/machine.js
+++ src/context/machine.js
@@ -42,11 +42,93 @@ const machine = createMachine(
       },
       player: {
         score: 0,
-        hand: []
+        hand: [
+          {
+            name: 'balas',
+            value: 0
+          },
+          {
+            name: 'escalera',
+            value: 0
+          },
+          {
+            name: 'cuadras',
+            value: 0
+          },
+          {
+            name: 'tontos',
+            value: 0
+          },
+          {
+            name: 'full',
+            value: 0
+          },
+          {
+            name: 'quinas',
+            value: 0
+          },
+          {
+            name: 'trenes',
+            value: 0
+          },
+          {
+            name: 'poker',
+            value: 0
+          },
+          {
+            name: 'senas',
+            value: 0
+          },
+          {
+            name: 'grande',
+            value: 0
+          }
+        ]
       },
       bot: {
         score: 0,
-        hand: []
+        hand: [
+          {
+            name: 'balas',
+            value: 0
+          },
+          {
+            name: 'escalera',
+            value: 0
+          },
+          {
+            name: 'cuadras',
+            value: 0
+          },
+          {
+            name: 'tontos',
+            value: 0
+          },
+          {
+            name: 'full',
+            value: 0
+          },
+          {
+            name: 'quinas',
+            value: 0
+          },
+          {
+            name: 'trenes',
+            value: 0
+          },
+          {
+            name: 'poker',
+            value: 0
+          },
+          {
+            name: 'senas',
+            value: 0
+          },
+          {
+            name: 'grande',
+            value: 0
+          }
+        ]
       }
     },
     states: {
@@ -75,7 +157,19 @@ const machine = createMachine(
             on: {
               ROLL: 'roll', //another name?,
               CANCEL: '#stateMachine.lobby'
-            }
+            },
+            entry: assign({
+              board: ({ board }) => ({
+                ...board,
+                rollCount: 0,
+                dices: board.dices.map(dice => ({
+                  ...dice,
+                  value: 0,
+                  locked: false,
+                  inverted: false
+                }))
+              })
+            })
           },
           roll: {
             on: {
@@ -119,7 +213,16 @@ const machine = createMachine(
           },
           handChoice: {
             on: {
-              //TODO continue...
+              HAND_CHOSEN: {
+                actions: assign({
+                  player: ({ player }, event) => ({
+                    hand: player.hand.map(hand =>
+                      hand.name === event.hand.name ? { ...hand, value: event.value } : hand
+                    ),
+                    score: player.score + event.value
+                  })
+                })
+              },
               END_TURN: '#stateMachine.playing',
               SURRENDER: '#stateMachine.playing',
               CANCEL: '#stateMachine.lobby'
@@ -134,25 +237,71 @@ const machine = createMachine(
             on: {
               ROLL: 'roll',
               CANCEL: '#stateMachine.lobby'
-            }
+            },
+            entry: assign({
+              board: ({ board }) => ({
+                ...board,
+                rollCount: 0,
+                dices: board.dices.map(dice => ({
+                  ...dice,
+                  value: 0,
+                  locked: false,
+                  inverted: false
+                }))
+              })
+            })
           },
           roll: {
             on: {
-              DICES_CHOICE: 'dicesChoice',
+              ROLLED: {
+                actions: assign({
+                  board: ({ board }, event) => ({
+                    ...board,
+                    dices: board.dices.map(dice =>
+                      dice.locked ? dice : { ...dice, value: event.dices[dice.id - 1] }
+                    )
+                  })
+                })
+              },
+              ROLL: {
+                target: 'roll',
+                cond: 'canRollAgain'
+              },
+              BLOCK_DICES: {
+                actions: assign({
+                  board: ({ board }, event) => ({
+                    ...board,
+                    ...(board.rollCount === 1 && {
+                      dices: board.dices.map(dice =>
+                        event.diceIds.includes(dice.id) ? { ...dice, locked: true } : dice
+                      )
+                    })
+                  })
+                })
+              },
               HAND_CHOICE: 'handChoice',
               SURRENDER: '#stateMachine.playing',
               CANCEL: '#stateMachine.lobby'
-            }
-          },
-          dicesChoice: {
-            on: {
-              HAND_CHOICE: 'handChoice',
-              SURRENDER: '#stateMachine.playing',
-              CANCEL: '#stateMachine.lobby'
-            }
+            },
+            entry: assign({
+              board: ({ board }) => ({
+                ...board,
+                rollCount: board.rollCount + 1
+              })
+            })
           },
           handChoice: {
             on: {
+              HAND_CHOSEN: {
+                actions: assign({
+                  bot: ({ bot }, event) => ({
+                    hand: bot.hand.map(hand =>
+                      hand.name === event.hand.name ? { ...hand, value: event.value } : hand
+                    ),
+                    score: bot.score + event.value
+                  })
+                })
+              },
               END_TURN: '#stateMachine.playing',
               SURRENDER: '#stateMachine.playing',
               CANCEL: '#stateMachine.lobby'
