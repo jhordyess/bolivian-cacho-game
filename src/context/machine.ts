0b040c3ed199ed43c511fd9622360aa6faaf4249
jhordyess <dev@jhordyess.com>
2024-12-10 10:29:38 -0400

    feat: Refactor state machine context and events into separate types for improved type safety and clarity

--- src/context/machine.ts
+++ src/context/machine.ts
@@ -1,88 +1,14 @@
 import { handCalculation } from '@/utils/handCalc'
 import { assign, setup } from 'xstate'
-import { Hand } from '@/enum'
+import { MachineEvents, MachineContext } from './types'
 
 const maxFlips = 2
 const maxBlocks = 2
 
 const machine = setup({
   types: {
-    context: {} as {
-      board: {
-        rollCount: number
-        flipCount: number
-        blockCount: number
-        dices: { id: number; value: number; inverted: boolean; locked: boolean }[]
-      }
-      player: {
-        score: number
-        hand: Record<Hand, number>
-        options: Record<Hand, number>
-      }
-      bot: {
-        score: number
-        hand: Record<Hand, number>
-        options: Record<Hand, number>
-      }
-    },
-    events: {} as
-      | {
-          type: 'START_GAME'
-        }
-      | {
-          type: 'IS_PLAYER_TURN'
-        }
-      | {
-          type: 'IS_BOT_TURN'
-        }
-      | {
-          type: 'GAME_OVER'
-        }
-      | {
-          type: 'CANCEL'
-        }
-      | {
-          type: 'ROLL'
-        }
-      | {
-          type: 'ROLLED'
-        }
-      | {
-          type: 'BLOCK_DICE'
-          diceId: number
-        }
-      | {
-          type: 'FLIP_DICE'
-          diceId: number
-        }
-      | {
-          type: 'HAND_CHOICE'
-        }
-      | {
-          type: 'SURRENDER'
-        }
-      | {
-          type: 'HAND_CHOSEN'
-          hand: Hand
-        }
-      | {
-          type: 'END_TURN'
-        }
-      | {
-          type: 'RESTART_GAME'
-        }
-      | {
-          type: 'ROLLDICES'
-          newDices: { id: number; value: number; inverted: boolean; locked: boolean }[]
-        }
-      | {
-          type: 'ROLLDICE'
-          dices: number[]
-        }
-      | {
-          type: 'BLOCK_DICES'
-          diceIds: number[]
-        }
+    context: {} as MachineContext,
+    events: {} as MachineEvents
   }
 }).createMachine({
   id: 'stateMachine',
