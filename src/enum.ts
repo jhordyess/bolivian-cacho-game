6759f6f59f560147e02342063266361e97155683
jhordyess <dev@jhordyess.com>
2024-12-10 00:44:29 -0400

    feat: Add Hand enum for improved type safety in game logic

---
+++ src/enum.ts
@@ -0,0 +1,12 @@
+export enum Hand {
+  balas = 'balas',
+  tontos = 'tontos',
+  trenes = 'trenes',
+  cuadras = 'cuadras',
+  quinas = 'quinas',
+  senas = 'senas',
+  escalera = 'escalera',
+  full = 'full',
+  poker = 'poker',
+  grande = 'grande'
+}
