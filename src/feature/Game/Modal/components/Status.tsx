40ac8de9ddce35e6462d63b8395b5702e9a2dcfc
jhordyess <dev@jhordyess.com>
2024-12-09 23:51:07 -0400

    feat: Convert status to enum for improved type safety in Status component

--- src/feature/Game/Modal/components/Status.tsx
+++ src/feature/Game/Modal/components/Status.tsx
@@ -1,9 +1,9 @@
-export const status = {
-  idle: 'idle',
-  rolling: 'rolling',
-  selecting: 'selecting'
+export enum status {
+  idle = 'idle',
+  rolling = 'rolling',
+  selecting = 'selecting'
 }
 
-export const Status = ({ state = 'idle' }) => {
+export const Status = ({ state = status.idle }: { state: status }) => {
   return <h1>{state}</h1>
 }
