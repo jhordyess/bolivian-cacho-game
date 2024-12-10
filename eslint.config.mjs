9fc9a5460442c23ad675d6913c49f746b7fe36b9
jhordyess <dev@jhordyess.com>
2024-12-09 23:10:10 -0400

    feat: Enhance ESLint configuration with TypeScript support and add react-hooks plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,23 @@
 import globals from 'globals'
 import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 import pluginReact from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    ignores: ['node_modules', 'dist']
-  },
-  { files: ['**/*.{js,mjs,cjs,jsx}'] },
+  { ignores: ['node_modules', 'dist', 'webpack.config.*.cjs'] },
+  { files: ['**/*.{ts,tsx}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   {
+    plugins: {
+      'react-hooks': reactHooks
+    },
     rules: {
-      'react/prop-types': 'off',
+      ...reactHooks.configs.recommended.rules,
       'react/react-in-jsx-scope': 'off'
     },
     settings: {
